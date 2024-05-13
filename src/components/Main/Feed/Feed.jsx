8066a68f6432b8108d2540d41f220ffb750fde2a
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-13 19:41:31 +0300

    Add responsive design for main sizes in feed section

--- src/components/Main/Feed/Feed.jsx
+++ src/components/Main/Feed/Feed.jsx
@@ -4,14 +4,20 @@ import MiniProfile from "../MiniProfile/MiniProfile";
 
 export default function Feed() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+    <main
+      className="grid grid-cols-1 md:max-w-6xl mx-auto
+    md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
+    >
       {/* Posts - left side */}
-      <section className="md:col-span-2">
+      <section className="md:col-span-2 lg:col-span-3 xl:col-span-4">
         <Posts />
       </section>
 
       {/* MiniProfile - right side */}
-      <section className="hidden md:inline-grid md:col-span-1">
+      <section
+        className="hidden md:inline-grid
+      lg:col-span-2 md:col-span-2 xl:col-span-1"
+      >
         <div className="fixed w-[380px]">
           <MiniProfile />
         </div>

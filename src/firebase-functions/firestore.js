03a6000fc2249bbd8e859d8e195a05b810a3e13c
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-13 16:01:36 +0300

    Add JSDoc for uploadPost to firestore function

--- src/firebase-functions/firestore.js
+++ src/firebase-functions/firestore.js
@@ -8,6 +8,16 @@ import {
 
 const db = getFirestore(app);
 
+/**
+ * Uploads a new post to the database asynchronously.
+ *
+ * @param {object} session - The user session object containing user information.
+ * @param {string} caption - The caption for the post.
+ * @param {string} imageUrl - The URL of the image to be included in the post.
+ * @param {Function} setPostUploading - Function to set the post uploading state (true/false).
+ * @param {Function} setOnUpload - Function to set the upload state of the modal (true/false).
+ * @returns {Promise<void>} A Promise that resolves when the post is successfully uploaded.
+ */
 export const uploadPost = async (
   session,
   caption,
@@ -15,7 +25,10 @@ export const uploadPost = async (
   setPostUploading,
   setOnUpload
 ) => {
+  // Set the post uploading state to true
   setPostUploading(true);
+
+  // Add a new document to the "posts" collection with post details
   const docRef = await addDoc(collection(db, "posts"), {
     username: session.user.username,
     profileImage: session.user.image,
@@ -23,6 +36,8 @@ export const uploadPost = async (
     image: imageUrl,
     createdOn: serverTimestamp(),
   });
+
+  // Set the post uploading and upload modal states to false
   setPostUploading(false);
   setOnUpload(false);
 };
